492aeefe42078d8d7ff09d95380b205042c03e48
Keith Kariru <karirukeith@gmail.com>
2022-10-25 10:19:33 +0300

    add messages into object

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+let messagesObject = {
+    sign: ["Aries", "Capricorn", "Taurus", "Scorpio"],
+    message: ["You are Strong", "You are Brave", "You are kind"],
+    mission: ["Keep on", "Work Harder", "Don't lose hope"]
+};
+
