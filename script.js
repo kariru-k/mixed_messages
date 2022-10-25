a8fc9c9d8e67022fdbc7fb51d152115bddac33b7
Keith Kariru <karirukeith@gmail.com>
2022-10-25 11:54:05 +0300

    finished mixed message generator

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ let messagesObject = {
     mission: ["Keep on", "Work Harder", "Don't lose hope"]
 };
 
+const createMessage = () => {
+    let signMessage = messagesObject.sign[Math.floor(Math.random() * messagesObject.sign.length)]
+    // console.log(signMessage)
+
+    let quote = messagesObject.message[Math.floor(Math.random() * messagesObject.message.length)]
+
+    let mission = messagesObject.mission[Math.floor(Math.random() * messagesObject.mission.length)]
+
+
+    let string = signMessage + '. ' + quote + '. ' + mission + '.'
+
+    return string
+}
+
+console.log(createMessage())
